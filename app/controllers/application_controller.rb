dc79adb514f7be6a9f3964b06d4b7c39b35db756
OlawaleJoseph <adedekojoseph@gmail.com>
2020-12-30 02:09:19 +0100

    add create trainer functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name username email password password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name username email password password_confirmation is_trainer speciality info])
   end
 end
--- spec/requests/api/v1/auth/register_spec.rb
+++ spec/requests/api/v1/auth/register_spec.rb
@@ -1,17 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'SignUp', type: :request do
-  context 'Validations' do
-    let(:parameters) do
-      { first_name: 'Jane',
-        last_name: 'Doe',
-        username: 'janedoe',
-        email: 'jane@example.com',
-        password: 'Password1',
-        confirm_password: 'Password1' }
-    end
+  let(:parameters) do
+    { first_name: 'Jane',
+      last_name: 'Doe',
+      username: 'janedoe',
+      email: 'jane@example.com',
+      password: 'Password1',
+      confirm_password: 'Password1' }
+  end
+  let(:trainer_parameters) do
+    {
+      email: 'testonly@email.com',
+      first_name: 'John',
+      last_name: 'Doe',
+      username: 'john_doe',
+      password: 'Password1',
+      confirm_password: 'Password1',
+      is_trainer: true,
+      speciality: 'Squats',
+      info: '5 years experirnce as a professional trainer'
+    }
+  end
+  context 'User#Validations' do
     let(:url) { post api_v1_user_registration_path, params: parameters }
-
     scenario 'should return 422 if first_name is absent' do
       parameters[:first_name] = nil
       url
@@ -36,7 +48,7 @@ RSpec.describe 'SignUp', type: :request do
       expect(json['errors']).to have_key('email')
     end
 
-    scenario 'should return 422 if first_name is absent' do
+    scenario 'should return 422 if password is absent' do
       parameters[:password] = nil
       url
       json = JSON.parse(response.body)
@@ -44,7 +56,7 @@ RSpec.describe 'SignUp', type: :request do
       expect(json['errors']).to have_key('password')
     end
 
-    scenario 'should return 422 if first_name is absent' do
+    scenario 'should return 422 if username is absent' do
       parameters[:username] = nil
       url
       json = JSON.parse(response.body)
@@ -83,5 +95,67 @@ RSpec.describe 'SignUp', type: :request do
       expect(response.headers['token-type']).to eq('Bearer')
       expect(json['errors']).to be_nil
     end
+  end
+
+  context 'Trainer#Validation' do
+    let(:url) { post api_v1_user_registration_path, params: trainer_parameters }
+    scenario 'should return 422 if speciality is absent' do
+      trainer_parameters[:speciality] = nil
+      url
+      json = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(json['errors']).to have_key('speciality')
+    end
+    scenario 'should return 422 if speciality if too short' do
+      trainer_parameters[:speciality] = 'ab'
+      url
+      json = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(json['errors']).to have_key('speciality')
+    end
+
+    scenario 'should return 422 if speciality if too long' do
+      trainer_parameters[:speciality] = 'a' * 51
+      url
+      json = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(json['errors']).to have_key('speciality')
+    end
+
+    scenario 'should return 422 if info is absent' do
+      trainer_parameters[:info] = nil
+      url
+      json = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(json['errors']).to have_key('info')
+    end
+    scenario 'should return 422 if info if too short' do
+      trainer_parameters[:info] = 'ab'
+      url
+      json = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(json['errors']).to have_key('info')
+    end
+
+    scenario 'should return 422 if info if too long' do
+      trainer_parameters[:info] = 'a' * 50_001
+      url
+      json = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(json['errors']).to have_key('info')
+    end
+
+    scenario 'should create a trainer' do
+      url
+      json = JSON.parse(response.body)
+
+      expect(response.headers['Authorization']).to be_truthy
+      expect(response.headers['uid']).to be_truthy
+      expect(response.headers['client']).to be_truthy
+      expect(response.headers['token-type']).to eq('Bearer')
+      expect(json['errors']).to be_nil
+      expect(json['data']['is_trainer']).to be true
+    end
+
   end
 end
